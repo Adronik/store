b6f4f17fee3f8daa0acc45e326e7738bcdcfbad6
 alex <alex@MacBook-Pro-alex.local>
2020-08-13 11:16:34 +0300

    Sienavigation fix

--- src/main/java/Utils/SiteNavigation.java
+++ src/main/java/Utils/SiteNavigation.java
@@ -8,13 +8,11 @@ public class SiteNavigation {
 
     public static Home openHomePage() {
         WebDriverManager.driver.get("http://automationpractice.com/index.php");
-        WebDriverWait wait = new WebDriverWait(WebDriverManager.driver, 5);
         return new Home();
     }
 
     public static void logoutURL() {
         WebDriverManager.driver.get("http://automationpractice.com/index.php?mylogout=");
-        WebDriverWait wait = new WebDriverWait(WebDriverManager.driver, 5);
     }
 
 }
